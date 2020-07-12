71f7dfade5d46a542106e4b9c6d09fd5eff4a2b2
adfr96 <alexdellorco@gmail.com>
2020-07-12 19:12:56 +0200

    avarage temperature with window operation

--- speed_layer/streamingApp.py
+++ speed_layer/streamingApp.py
@@ -13,26 +13,22 @@ if __name__ == "__main__":
     ssc = StreamingContext(sc, 5) # 5 second window    
 
     d_stream = ssc.socketTextStream('localhost',2020)
+    ssc.checkpoint(PROJ_DIR+'data/checkpoint/')
     d_stream = d_stream.flatMap(lambda row: row.split('\n'))
     d_stream = d_stream.map(lambda row: json.loads(row))
 
     """
     ELABORAZIONE DATI
     """
-    count = d_stream.count()
+    
+    
     temp_stream = d_stream.map(lambda row: toCelsius(row['temp']))
-    sum_temp = temp_stream.map(lambda a: (a,1)).reduce(lambda a,b: (a[0]+b[0],a[1]+b[1]))
-    #sum_temp.pprint()
-    list_avg = []
+    sum_temp = temp_stream.map(lambda a: (a,1)).reduceByWindow(lambda a,b: (a[0]+b[0],a[1]+b[1]),invReduceFunc=None,windowDuration=30,slideDuration=5)
+    
     avg_temp = sum_temp.map(lambda a: a[0]/a[1])
     
-    #avg_temp.foreachRDD(lambda rdd: list_avg.append(rdd.collect()))
-    
     avg_temp.pprint()
-    avg_temp.saveAsTextFiles(PROJ_DIR+'data/output/test/')
-    #d_stream.pprint()
-    #d_stream.saveAsTextFiles('output/')
+    #avg_temp.saveAsTextFiles(PROJ_DIR+'data/output/test/')
     
     ssc.start()
     ssc.awaitTermination(40)
-    print(list_avg)
