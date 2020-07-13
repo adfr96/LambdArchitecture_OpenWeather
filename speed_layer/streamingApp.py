de72efc45606dc4359c63ee537c850c63ce7651d
adfr96 <alexdellorco@gmail.com>
2020-07-13 12:14:30 +0200

    add strong wind check

--- speed_layer/streamingApp.py
+++ speed_layer/streamingApp.py
@@ -2,8 +2,11 @@ from pyspark import SparkContext, SparkConf
 from pyspark.streaming import StreamingContext
 from confluent_kafka import Consumer
 import json
+import sys
 
-PROJ_DIR = '/home/adfr/Documenti/python-BigData/progetto2/'
+PROJ_DIR = sys.argv[1]
+
+WIND_THRESHOLD = 4
 
 def toCelsius(temp):
     return float(temp)-273.15
@@ -20,16 +23,27 @@ if __name__ == "__main__":
     """
     ELABORAZIONE DATI
     """
+    def sum_func(a,b):
+        return (a[0]+b[0],a[1]+b[1])
+    def diff_func(a,b):
+        return (a[0]-b[0],a[1]-b[1])
     
-    
+    #MEDIA TEMPERATURA PER REGIONE
+
     temp_stream = d_stream.map(lambda row: (row['regione'],float(toCelsius(row['temp']))))
     #sum_temp = temp_stream.map(lambda a: (a,1)).reduceByWindow(lambda a,b: (a[0]+b[0],a[1]+b[1]),invReduceFunc=None,windowDuration=30,slideDuration=5)
-    sum_temp = temp_stream.map(lambda r_t: (r_t[0],(r_t[1],1))).reduceByKeyAndWindow(lambda a,b: (a[0]+b[0],a[1]+b[1]),invFunc=None,windowDuration=30,slideDuration=5)
-    
+    sum_temp = temp_stream.map(lambda r_t: (r_t[0],(r_t[1],1))).reduceByKeyAndWindow(sum_func,invFunc=diff_func,windowDuration=30,slideDuration=5)
+    #sum_temp.saveAsTextFiles(PROJ_DIR+'data/output/test/')
     avg_temp = sum_temp.map(lambda a: (a[0],a[1][0]/a[1][1]))
     
-    avg_temp.pprint()
-    #avg_temp.saveAsTextFiles(PROJ_DIR+'data/output/test/')
+    avg_temp.pprint(num=1000)
+    avg_temp.saveAsTextFiles(PROJ_DIR+'data/output/avg_temp/')
     
+    #CONTROLLO VENTO FORTE PER CITTA
+    wind_stream = d_stream.map(lambda row: (row['citta'],(row['wind_speed'],row['wind_deg'])))
+    wind_stream = wind_stream.filter(lambda c_w: c_w[1][0]>WIND_THRESHOLD)
+    wind_stream.pprint(num=1000)
+    wind_stream.saveAsTextFiles(PROJ_DIR+'data/output/wind/')
+
     ssc.start()
     ssc.awaitTermination(40)
