d7110780129ff83f9626926a5ffd7a4c50eae5e1
adfr96 <alexdellorco@gmail.com>
2020-07-14 12:48:41 +0200

    tuple -> dict, and refactoring

--- speed_layer/streamingApp.py
+++ speed_layer/streamingApp.py
@@ -14,14 +14,16 @@ WIND_THRESHOLD = 4
 def toCelsius(temp):
     return float(temp)-273.15
 
-def getSparkSessionInstance(sparkConf):
-    if ("sparkSessionSingletonInstance" not in globals()):
-        globals()["sparkSessionSingletonInstance"] = SparkSession \
-            .builder \
-            .config(conf=sparkConf) \
-            .getOrCreate()
-    return globals()["sparkSessionSingletonInstance"]
 
+def show_wind_dataframe(rdd):
+        if(rdd.count() > 0 ):
+            spark = SparkSession.builder.getOrCreate()
+
+            rowRdd = rdd.map(lambda w: Row(city=w['citta'],wind_speed = w['wind_speed'], wind_deg = w['wind_deg'], date = w['date']))
+            wind_dataframe = spark.createDataFrame(rowRdd)
+
+            #print("stampa del dataframe")
+            wind_dataframe.show()
 if __name__ == "__main__":
     sc = SparkContext(appName="PythonStreamingRecieverKafkaWordCount")
     ssc = StreamingContext(sc, 5) # 5 second window    
@@ -42,41 +44,23 @@ if __name__ == "__main__":
     #MEDIA TEMPERATURA PER REGIONE
 
     temp_stream = d_stream.map(lambda row: (row['regione'],float(toCelsius(row['temp']))))
-    #sum_temp = temp_stream.map(lambda a: (a,1)).reduceByWindow(lambda a,b: (a[0]+b[0],a[1]+b[1]),invReduceFunc=None,windowDuration=30,slideDuration=5)
     sum_temp = temp_stream.map(lambda r_t: (r_t[0],(r_t[1],1))).reduceByKeyAndWindow(sum_func,invFunc=diff_func,windowDuration=30,slideDuration=5)
     #sum_temp.saveAsTextFiles(PROJ_DIR+'data/output/test/')
-    avg_temp = sum_temp.filter(lambda a: a[1][1]>0).map(lambda a: (a[0],a[1][0]/a[1][1]))
-
+    avg_temp = sum_temp.filter(lambda a: a[1][1]>0).map(lambda a: {'regione':a[0],'media_temp':a[1][0]/a[1][1]})
     avg_temp.pprint()
     #avg_temp.saveAsTextFiles(PROJ_DIR+'data/output/test/')
-    #d_stream.pprint()
-    #d_stream.saveAsTextFiles('output/'))
 
     avg_temp.pprint(num=1000)
     avg_temp.saveAsTextFiles(PROJ_DIR+'data/output/avg_temp/')
     
     #CONTROLLO VENTO FORTE PER CITTA
-    wind_stream = d_stream.map(lambda row: (row['citta'],(row['wind_speed'],row['wind_deg'])))
-    wind_stream = wind_stream.filter(lambda c_w: c_w[1][0]>WIND_THRESHOLD)
+    #TODO convertire data
+    wind_stream = d_stream.map(lambda row: {'citta':row['citta'],'wind_speed':row['wind_speed'],'wind_deg':row['wind_deg'],'date': row['datetime']})
+    wind_stream = wind_stream.filter(lambda c_w: c_w['wind_speed']>WIND_THRESHOLD)
     #wind_stream.pprint(num=1000)
     #wind_stream.saveAsTextFiles(PROJ_DIR+'data/output/wind/')
-
-    def process(rdd):
-        #spark = getSparkSessionInstance(rdd.contex.getConf())
-        if(rdd.count() > 0 ):
-            spark = SparkSession.builder.getOrCreate()
-
-            rowRdd = rdd.map(lambda c_w: Row(city=c_w[0],wind_speed = c_w[1][0], wind_deg = c_w[1][1]))
-            
-            wind_dataframe = spark.createDataFrame(rowRdd)
-
-            #wind_dataframe.createOrReplaceTempView("wind")
-
-            #wind_query = spark.sql("select * from wind")
-            print("stampa del dataframe")
-            wind_dataframe.show()
-    
-    wind_stream.foreachRDD(process)
+   
+    wind_stream.foreachRDD(show_wind_dataframe)
         
 
     ssc.start()
