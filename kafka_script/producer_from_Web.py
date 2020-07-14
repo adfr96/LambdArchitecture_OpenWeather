a8d8a6f903816ac3fd28b8ed5e48a820aafb02b9
adfr96 <alexdellorco@gmail.com>
2020-07-14 18:39:43 +0200

    move data cleaning to producers

--- kafka_script/producer_from_Web.py
+++ kafka_script/producer_from_Web.py
@@ -2,8 +2,10 @@ from confluent_kafka import Producer
 import time
 import requests
 import json
+import jsons
 import pandas as pd
 import sys
+import datetime
 
 PROJ_DIR = sys.argv[1]
 appId = sys.argv[2]
@@ -15,6 +17,12 @@ def delivery_report(err, msg):
         print('Message delivery failed: {}'.format(err))
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
+
+def toCelsius(temp):
+    return float(temp)-273.15
+
+def conv_date(date):
+    return datetime.datetime.fromtimestamp(date)
 
 def getLineFromWebSocket(provincia):
     param_request = f'http://api.openweathermap.org/data/2.5/weather?id={provincia[3]}&appid={appId}&lang=it'
@@ -40,10 +48,10 @@ def getLineFromWebSocket(provincia):
         row["weather_main"]="Not Defined"
         row["weather_description"]="Not Defined"
             
-    row["temp"]=data["main"]["temp"]
+    row["temp"]=toCelsius(data["main"]["temp"])
     row["feels_like"]=data["main"]["feels_like"]
-    row["temp_min"]=data["main"]["temp_min"]
-    row["temp_max"]=data["main"]["temp_max"]
+    row["temp_min"]=toCelsius(data["main"]["temp_min"])
+    row["temp_max"]=toCelsius(data["main"]["temp_max"])
     row["pressure"]=data["main"]["pressure"]
     row["humidity"]=data["main"]["humidity"]
 
@@ -85,9 +93,10 @@ def getLineFromWebSocket(provincia):
         row["snow_1h"]=0
         row["snow_3h"]=0
 
-    row["datetime"] = data["dt"]	
+    #row["datetime"] = data["dt"]
+    row["datetime"] = conv_date(data["dt"])	
 
-    return (json.dumps(row))
+    return (jsons.dumps(row))
 
 def getProvincia(id_provincie):
     if(INDEX == len(id_provincie)):
--- kafka_script/producer_from_file.py
+++ kafka_script/producer_from_file.py
@@ -1,7 +1,9 @@
 from confluent_kafka import Producer
 import time
 import json
+import jsons
 import sys
+import datetime
 
 def delivery_report(err, msg):
     """ Called once for each message produced to indicate delivery result.
@@ -11,17 +13,29 @@ def delivery_report(err, msg):
     else:
         print('Message delivered to {} [{}]'.format(msg.topic(), msg.partition()))
 
+def toCelsius(temp):
+    return float(temp)-273.15
+
+def conv_date(date):
+    return datetime.datetime.fromtimestamp(date)
+
 path_file = sys.argv[1]
 p = Producer({'bootstrap.servers': 'localhost:9092'})
 file_name = path_file+'dati_meteo.json'
+#file_name = path_file+'mini_data.json'
 
 file_open = open(file_name,'r')
 
 for line in file_open:
-    #row = json.load(line)
-    print(f'Linea letta: {line}\n')
+    row = json.loads(line)
+    row["temp"]=toCelsius(row["temp"])
+    row["temp_min"]=toCelsius(row["temp_min"])
+    row["temp_max"]=toCelsius(row["temp_max"])
+    row["datetime"] = conv_date(row["datetime"])	
+    row_string = jsons.dumps(row)
+    print(f'Linea letta: {row_string}\n')
     p.poll(0)
-    p.produce('test', line.encode('utf-8'), callback=delivery_report)
+    p.produce('test', row_string.encode('utf-8'), callback=delivery_report)
 
     time.sleep(1)
 
