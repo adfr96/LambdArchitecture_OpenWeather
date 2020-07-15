cf48ee00a5ae66fb54479d35eb3f61a4b31bc154
adfr96 <alexdellorco@gmail.com>
2020-07-15 12:40:48 +0200

    modifed propertis

--- propertis.py
+++
@@ -1,3 +0,0 @@
-TTL = 50  # time (in second) to live application
-PROJ_DIR = '/home/adfr/Documenti/python-BigData/progetto2/'  # home directory of project
-PORT_CONSUMER_TO_STREAMING = 2020  # number of port for communication
---
+++ propertis_template.py
@@ -0,0 +1,7 @@
+"""
+create a propertis.py file from this template
+
+TTL =  # time (in second) to live application
+PROJ_DIR =   # home directory of project
+PORT_CONSUMER_TO_STREAMING =   # number of port for communication
+"""
