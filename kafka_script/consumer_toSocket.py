180267dbfdf7b1bc4a12c0079b578774022be3c5
adfr96 <alexdellorco@gmail.com>
2020-07-15 12:31:31 +0200

    some little fix

--- kafka_script/consumer_toSocket.py
+++ kafka_script/consumer_toSocket.py
@@ -4,10 +4,11 @@ import sys
 import time
 import json
 
+from LambdArchitecture_OpenWeather.propertis import PORT_CONSUMER_TO_STREAMING
+
 HOST = 'localhost'
-PORT = 2020
-address = (HOST,PORT)
-name  = sys.argv[1]
+address = (HOST, PORT_CONSUMER_TO_STREAMING)
+name = sys.argv[1]
 
 c = Consumer({
     'bootstrap.servers': 'localhost:9092',
@@ -16,19 +17,19 @@ c = Consumer({
 })
 
 c.subscribe(['test'])
-s = socket.socket(family= socket.AF_INET, type= socket.SOCK_STREAM)
-s.bind((HOST,PORT))
-s.setsockopt( socket.SOL_SOCKET, socket.SO_SNDBUF, 1024)
+s = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
+s.bind(address)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_SNDBUF, 1024)
 print("prima della connessione")
 s.listen()
-conn,addr = s.accept() 
+conn, addr = s.accept()
 print("dopo la connessione")
 
 start_time = time.time()
 now = start_time
 
-while now<start_time+30:
-    
+while now < start_time + 30:
+
     now = time.time()
     msg = c.poll(1.0)
 
@@ -38,12 +39,12 @@ while now<start_time+30:
     if msg.error():
         print("Consumer error: {}".format(msg.error()))
         continue
-    
+
     row = json.loads(msg.value().decode('utf-8'))
-    message = json.dumps(row)+'\n'
+    message = json.dumps(row) + '\n'
     sended = conn.send(message.encode())
     print("sended message to socket")
-    #print('Received message: {}'.format(msg.value().decode('utf-8')))
-    
+    # print('Received message: {}'.format(msg.value().decode('utf-8')))
+
 s.close()
-c.close() 
+c.close()
