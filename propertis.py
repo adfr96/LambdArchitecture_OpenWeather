9bac4631e63740dd4a09b25d5bfc3ad6f430a16e
adfr96 <alexdellorco@gmail.com>
2020-07-23 10:12:37 +0200

    update .gitignore

--- propertis.py
+++
@@ -1,6 +0,0 @@
-TTL =  180# time (in second) to live application
-PROJ_DIR =   "/home/giacomo/PycharmProjects/LambdArchitecture_OpenWeather"# home directory of project
-PORT_CONSUMER_TO_STREAMING =  2020 # number of port for communication
-BATCH_DURATION =  10#batch duration of streaming app
-WINDOW_DURATION =  10#windows lenght in streaming app
-SLIDE_DURATION =  10#slide duration of window in streaming app, must be a multiple of BATCH_DURATION
